2abed04c2b98a52167495a5242a016f18a050380
Alex Ghit <76708587+alexghit@users.noreply.github.com>
2025-11-23 05:50:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const base = "https://cdn.jsdelivr.net/gh/AlexGhit/marketing-flamingo";
 
+// Utility to load JS
+function loadJS(file) {
+  const s = document.createElement("script");
+  s.src = `${base}/src/${file}`;
+  s.defer = true;
+  document.body.appendChild(s);
+}
+
+// Auto-load all files in /src
+const files = [
+  "lang-switch.js",
+
+];
+
+files.forEach(file => loadJS(file));
